d99d09a96a191564766248b83d76ba0c08fb4268
Simo672k <mohammed.hakmiii555@gmail.com>
2023-11-10 23:59:41 +0100

    login page ui

--- src/components/Fileds/GroupInput.tsx
+++ src/components/Fileds/GroupInput.tsx
@@ -7,7 +7,7 @@ interface PropType {
 
 function GroupInput({children}: PropType) {
   return (
-    <div className="bg-white flex items-center px-4 py-1 border-2 rounded">{children}</div>
+    <div className="bg-white flex items-center px-4 py-1 border-2 border-gray-300 focus-within:border-blue-400 focus-within:shadow-[0_0_3px_4px_var(--tw-shadow-colored)] focus-within:shadow-blue-200 rounded">{children}</div>
   )
 }
 export default GroupInput
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,31 +1,49 @@
+import { useRef, useState } from 'react';
 import * as Form from '@radix-ui/react-form';
-import {RxLockClosed, RxPerson, RxEyeClosed} from 'react-icons/rx';
+import {RxLockClosed, RxPerson, RxEyeClosed, RxEyeOpen} from 'react-icons/rx';
 import GroupInput from '../components/Fileds/GroupInput';
 
 function Login() {
+  const [showPassword, setShowPassword]= useState(false);
+  const passwordRef= useRef<HTMLInputElement>(null);
+
+
   return (
     <div className='w-full h-full flex items-center justify-center'>
-      <Form.Root>
-        <Form.Field name='username'>
-          <Form.Label className='block uppercase text-sm font-semibold ml-4 mb-1'>User</Form.Label>
-          <GroupInput>
-            <RxPerson className="mr-3"/>
-            <Form.Control asChild>
-              <input className='outline-0' type="text" placeholder='Enter Your Username'/>
-            </Form.Control>
-          </GroupInput>
-        </Form.Field>
-        <Form.Field name='Password' className='mt-4'>
-          <Form.Label className='block uppercase text-sm font-semibold ml-4 mb-1'>Password</Form.Label>
-          <GroupInput>
-            <RxLockClosed className="mr-3"/>
-            <Form.Control asChild>
-              <input className='outline-0' type="password" placeholder='Enter Your Password'/>
-            </Form.Control>
-            <RxEyeClosed/>
-          </GroupInput>
-        </Form.Field>
-      </Form.Root>
+      <div className='border-2 px-8 py-12 rounded-lg bg-white'>
+        <h2 className='text-2xl text-center pb-10 font-semibold'>Login</h2>
+        <Form.Root>
+          <Form.Field name='username'>
+            <Form.Label className='block uppercase text-sm font-semibold tracking-wider ml-4 mb-2'>User</Form.Label>
+            <GroupInput>
+              <RxPerson className="mr-3"/>
+              <Form.Control asChild>
+                <input className='outline-0' type="text" placeholder='Enter Your Username'/>
+              </Form.Control>
+            </GroupInput>
+          </Form.Field>
+          <Form.Field name='Password' className='mt-4'>
+            <Form.Label className='block uppercase text-sm font-semibold tracking-wider ml-4 mb-2'>Password</Form.Label>
+            <GroupInput>
+              <RxLockClosed className="mr-3"/>
+              <Form.Control asChild>
+                <input ref={passwordRef} className='outline-0' type={showPassword? "text" :"password"} placeholder='Enter Your Password'/>
+              </Form.Control>
+              <button type='button' onClick={()=>{
+                setShowPassword(!showPassword);
+                passwordRef.current?.focus()
+              }}>
+                {
+                  showPassword?
+                  <RxEyeClosed/>:
+                  <RxEyeOpen/>
+                }
+              </button>
+            </GroupInput>
+          </Form.Field>
+          <Form.Submit className='block bg-blue-600 mx-auto mt-6 w-full text-white px-12 py-1 rounded hover:bg-blue-500'>Login</Form.Submit>
+        </Form.Root>
+      </div>
     </div>
   )
 }
