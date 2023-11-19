a1bc741a95c6c8921d82ed0d723071346b967fc2
Simo672k <mohammed.hakmiii555@gmail.com>
2023-11-19 16:46:12 +0100

    minor fix of sidebar link

--- src/components/Sidebar/SideBarNavLink.tsx
+++ src/components/Sidebar/SideBarNavLink.tsx
@@ -3,7 +3,7 @@ import { LinkProps } from "../../utils/types"
 function SideBarNavLink({children, active= false, className}: LinkProps) {
   return (
     <li className="mb-2">
-      <a href="#" className={`flex items-center hover:text-blue-700 hover:font-semibold${active && " text-blue-700 font-semibold" + className|| ''}`}>
+      <a href="#" className={`flex items-center hover:text-blue-700 hover:font-semibold ${active && " text-blue-700 font-semibold" + (className ?? '')}`}>
         {children}
       </a>
     </li>
